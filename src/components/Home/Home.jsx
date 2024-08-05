53879abecabf73112477b94e8b16c13db705234e
Rejina Khadka <rejinakhadka00@gmail.com>
2024-08-05 10:54:36 +0545

    feat(Folder-structutre)

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <header className="border-b border-gray-200">
+      <div className=" size h-[60px] flex items-center justify-between">
+        <div className="flex items-center gap-3"></div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
